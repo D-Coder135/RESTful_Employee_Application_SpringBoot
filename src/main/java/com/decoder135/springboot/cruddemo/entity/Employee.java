22f9a596a2779f603d4e6f8761d04be2d954a8dd
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:23:55 +0530

    add the @Column annotations

--- src/main/java/com/decoder135/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/decoder135/springboot/cruddemo/entity/Employee.java
@@ -15,8 +15,10 @@ public class Employee {
     @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "email")
     private String email;
 
     // define constructor
