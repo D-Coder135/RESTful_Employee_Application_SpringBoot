a735538e5d846286f62936436ee9020ecbace2a4
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:35:33 +0530

    add the @Repository annotation for the class EmployeeDAOJpaImpl.java

--- src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -1,9 +1,11 @@
 package com.decoder135.springboot.cruddemo.dao;
 
 import com.decoder135.springboot.cruddemo.entity.Employee;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO{
     @Override
     public List<Employee> findAll() {
