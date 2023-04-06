62633ab9c82e45596092639536505c0ef33dd986
D-Coder135 <devansh1352000@gmail.com>
2023-04-07 00:38:13 +0530

    update the return statement

--- src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
@@ -25,7 +25,7 @@ public class EmployeeRestController {
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
