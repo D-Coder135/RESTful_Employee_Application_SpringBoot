30798e249cc7211ca626ca511443fe2f57f6f150
D-Coder135 <devansh1352000@gmail.com>
2023-04-07 00:34:31 +0530

    add the @Service annotation

--- src/main/java/com/decoder135/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/decoder135/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,9 +1,11 @@
 package com.decoder135.springboot.cruddemo.service;
 
 import com.decoder135.springboot.cruddemo.entity.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
