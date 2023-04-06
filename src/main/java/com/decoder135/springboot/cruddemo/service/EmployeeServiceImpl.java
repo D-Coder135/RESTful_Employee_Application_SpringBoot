fd1bac65ed67711d009386a6851857c6e643d062
D-Coder135 <devansh1352000@gmail.com>
2023-04-07 00:36:04 +0530

    add the @Autowired annotation for the parametrized constructor

--- src/main/java/com/decoder135/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/decoder135/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.decoder135.springboot.cruddemo.service;
 
 import com.decoder135.springboot.cruddemo.dao.EmployeeDAO;
 import com.decoder135.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     private EmployeeDAO employeeDAO;
 
+    @Autowired
     public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
     }
