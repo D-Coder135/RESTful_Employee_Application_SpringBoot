9a74c79b1be12589822dd57c670f3f15a46be5ab
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:35:02 +0530

    override the required methods

--- src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -1,4 +1,12 @@
 package com.decoder135.springboot.cruddemo.dao;
 
+import com.decoder135.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
 public class EmployeeDAOJpaImpl implements EmployeeDAO{
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
 }
