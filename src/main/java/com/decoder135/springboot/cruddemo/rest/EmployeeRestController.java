221c19a16c1289b5d44926c0fc5e25f006e5382b
D-Coder135 <devansh1352000@gmail.com>
2023-04-07 00:37:19 +0530

    define a field of type EmployeeService

--- src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.decoder135.springboot.cruddemo.rest;
 
 import com.decoder135.springboot.cruddemo.dao.EmployeeDAO;
 import com.decoder135.springboot.cruddemo.entity.Employee;
+import com.decoder135.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     // quick and dirty: inject employee dao(use constructor injection)
 
