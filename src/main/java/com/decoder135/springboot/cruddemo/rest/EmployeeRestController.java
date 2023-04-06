034101c5f954260cd6d9b1d9d8b5f1fd97f01655
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:55:36 +0530

    initialize a private field of type EmployeeDAO

--- src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,5 +1,6 @@
 package com.decoder135.springboot.cruddemo.rest;
 
+import com.decoder135.springboot.cruddemo.dao.EmployeeDAO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
+    private EmployeeDAO employeeDAO;
     // quick and dirty: inject employee dao
 
     // expose "/employees" and return a list of employees
