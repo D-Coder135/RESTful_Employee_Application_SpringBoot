8fa5f9ba79a2b145a06d8423eec189e0f0295ee2
D-Coder135 <devansh1352000@gmail.com>
2023-04-07 00:34:09 +0530

    override the required method

--- src/main/java/com/decoder135/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/decoder135/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,4 +1,12 @@
 package com.decoder135.springboot.cruddemo.service;
 
+import com.decoder135.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
 public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
 }
