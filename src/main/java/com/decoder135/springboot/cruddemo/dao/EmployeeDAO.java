48325f34bc450a3e307f7a3550cd365d52d8e7dd
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:33:37 +0530

    define a method findAll() of type List<Employee>

--- src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeDAO.java
@@ -1,4 +1,11 @@
 package com.decoder135.springboot.cruddemo.dao;
 
+import com.decoder135.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
 }
