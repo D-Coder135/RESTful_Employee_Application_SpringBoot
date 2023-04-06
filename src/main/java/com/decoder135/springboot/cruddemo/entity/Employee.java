a713939f3a620455e2bb7f80ca012ace3f83d888
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:18:50 +0530

    add the @Entity and @Table annotation

--- src/main/java/com/decoder135/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/decoder135/springboot/cruddemo/entity/Employee.java
@@ -1,4 +1,9 @@
 package com.decoder135.springboot.cruddemo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 }
