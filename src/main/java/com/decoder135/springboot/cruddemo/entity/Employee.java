9a17953fa40db0a6d90012e864ec4e12db457b38
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:24:57 +0530

    add the parametrized constructor inside which initialize the private fields

--- src/main/java/com/decoder135/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/decoder135/springboot/cruddemo/entity/Employee.java
@@ -27,6 +27,13 @@ public class Employee {
 
     }
 
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     // define getter/setter
 
     // define toString
