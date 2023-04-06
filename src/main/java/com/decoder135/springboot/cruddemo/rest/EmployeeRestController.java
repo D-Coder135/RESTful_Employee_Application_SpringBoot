f19a3441ab0dc496a44fe72d0f7e39173382b2f8
D-Coder135 <devansh1352000@gmail.com>
2023-04-06 20:52:49 +0530

    add some quick comment inside the code

--- src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/decoder135/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,4 +1,9 @@
 package com.decoder135.springboot.cruddemo.rest;
 
 public class EmployeeRestController {
+
+    // quick and dirty: inject employee dao
+
+    // expose "/employees" and return a list of employees
+
 }
