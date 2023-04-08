8ae00a64a4c6edc25170c6dabb5b51951336e5c3
DevanshShukla <devansh1352000@gmail.com>
2023-04-08 19:40:27 +0530

    extend the JpaRepository<Employee, Integer> interface inside the EmployeeRepository interface

--- src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.decoder135.springboot.cruddemo.dao;
 
-public interface EmployeeRepository {
+import com.decoder135.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
 }
