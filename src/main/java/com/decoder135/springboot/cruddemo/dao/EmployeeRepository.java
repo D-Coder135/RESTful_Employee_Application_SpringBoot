f7661687f80f9465fbaf539b74a6fca5161d12cd
DevanshShukla <devansh1352000@gmail.com>
2023-04-09 00:33:36 +0530

    add some comment inside the code

--- src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/decoder135/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.decoder135.springboot.cruddemo.dao;
 import com.decoder135.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Spring provides an annotation for the changing the default endpoint i.e, in this case "/employees"
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it....no need to write any code LOL!
